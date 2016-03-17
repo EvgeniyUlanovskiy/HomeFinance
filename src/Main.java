d2285a59d882c5b4a3a4fd2b287c65287e0800ee
EvgeniyUlanovskiy <e.ulanovskiy@yandex.ru>
2016-03-17 13:57:28 +0500

    Added:
    -Maven
    
    -H2
    
    -Hibernate

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+
 public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception{
@@ -16,5 +18,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
     }
 }
