3917891623f6777d65afbbbf62aea27a494a6430
EvgeniyUlanovskiy <e.ulanovski@gmail.com>
2016-06-15 19:22:53 +0500

    ***

--- src/model/ComboBoxCollections.java
+++ src/model/ComboBoxCollections.java
@@ -40,27 +40,32 @@ public class ComboBoxCollections {
     );
 
     public static ObservableList<String> setMonthComBox = FXCollections.observableArrayList(
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12"
+            "января",
+            "февраля",
+            "марта",
+            "апреля",
+            "мая",
+            "июня",
+            "июля",
+            "августа",
+            "сентября",
+            "октября",
+            "ноября",
+            "декабря"
     );
 
     public static ObservableList<String> setYearComBox = FXCollections.observableArrayList(
-            "2015",
             "2016",
             "2017",
             "2018",
             "2019",
-            "2020"
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025",
+            "2026"
     );
 
     public static ObservableList<String> setCategorySelectComBox = FXCollections.observableArrayList(
